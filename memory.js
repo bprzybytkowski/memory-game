43f838d57c2b03fc6f8ab71be47809a7b799c864
Bartosz Przybytkowski <bprzybytkowski@gmail.com>
2016-05-15 18:38:48 +0200

    Overall styling of the game and adding tiles transitions

--- memory.js
+++ memory.js
@@ -3,14 +3,14 @@ function memory() {
 
     var domBoard = document.getElementById('board');
     var moves = 0;
-    var imagesArray = ['images/1.jpg', 'images/1.jpg',
-        'images/2.jpg', 'images/2.jpg',
-        'images/3.jpg', 'images/3.jpg',
-        'images/4.jpg', 'images/4.jpg',
-        'images/5.jpg', 'images/5.jpg',
-        'images/6.jpg', 'images/6.jpg',
-        'images/7.jpg', 'images/7.jpg',
-        'images/8.jpg', 'images/8.jpg'
+    var imagesArray = ['images/1.png', 'images/1.png',
+        'images/2.png', 'images/2.png',
+        'images/3.png', 'images/3.png',
+        'images/4.png', 'images/4.png',
+        'images/5.png', 'images/5.png',
+        'images/6.png', 'images/6.png',
+        'images/7.png', 'images/7.png',
+        'images/8.png', 'images/8.png'
     ];
     var tileClick = null;
     var movesCount = 0;
@@ -22,10 +22,33 @@ function memory() {
             domTextAddr,
             domTBODY;
 
+        function generateTiles() {
+
+            for (iY = 0; iY <= height; iY++) {
+                domTR = document.createElement('tr');
+                domTBODY.appendChild(domTR);
+
+                for (iX = 0; iX <= width; iX++) {
+                    domTD = document.createElement('td');
+                    domTR.appendChild(domTD);
+
+                    domTextAddr = document.createTextNode(iX + 'x' + iY);
+                    var randomImage = Math.floor(Math.random() * imagesArray.length);
+                    var randomSplice = imagesArray.splice(randomImage, 1);
+                    domTD.id = 'tile' + iX + 'x' + iY;
+                    domTD.innerHTML = '<img src=' + randomSplice + '>';
+                    domTD.addEventListener('click', flip, true);
+                    domTD.addEventListener('click', addScore, true);
+
+                }
+            }
+        }
+
         domTBODY = document.createElement('tbody');
 
         function flip() {
-            document.querySelector("#" + this.id + ' img').style.display = 'block';
+            document.querySelector("#" + this.id + ' img').style.visibility = 'visible';
+            document.querySelector("#" + this.id + ' img').style.opacity = '1';
             if (tileClick == null) {
                 tileClick = this;
             } else {
@@ -40,12 +63,14 @@ function memory() {
                     var secondClick = this;
 
                     function flipAgain() {
-                        document.querySelector("#" + secondClick.id + ' img').style.display = 'none';
-                        document.querySelector("#" + tileClick.id + ' img').style.display = 'none';
+                    	document.querySelector("#" + secondClick.id + ' img').style.visibility = 'hidden';
+                        document.querySelector("#" + secondClick.id + ' img').style.opacity = '0';
+                        document.querySelector("#" + tileClick.id + ' img').style.visibility = 'hidden';
+                        document.querySelector("#" + tileClick.id + ' img').style.opacity = '0';
                         tileClick = null;
                     }
 
-                    setTimeout(flipAgain, 500);
+                    setTimeout(flipAgain, 400);
                 }
 
             }
@@ -62,41 +87,13 @@ function memory() {
             document.querySelector("#score").innerHTML = movesCount;
         }
 
-
-        for (iY = 0; iY <= height; iY++) {
-            domTR = document.createElement('tr');
-            domTBODY.appendChild(domTR);
-
-            for (iX = 0; iX <= width; iX++) {
-                domTD = document.createElement('td');
-                domTR.appendChild(domTD);
-
-                domTextAddr = document.createTextNode(iX + 'x' + iY);
-                var randomImage = Math.floor(Math.random() * imagesArray.length);
-                var randomSplice = imagesArray.splice(randomImage, 1);
-                domTD.id = 'pole' + iX + 'x' + iY;
-                domTD.innerHTML = '<img src=' + randomSplice + '>';
-                domTD.addEventListener('click', flip, true);
-                domTD.addEventListener('click', addScore, true);
-
-            }
-        }
+        generateTiles();
 
         domBoard.appendChild(domTBODY);
+        
     }
 
     generateBoard(3, 3);
-
-
-    /*function addScore() {
-        score++; // score=score+1;
-
-        if (!(score % 10) && speed > 100) {
-            speed = speed - 100;
-        }
-
-        document.querySelector('#score').innerHTML = score;
-    }*/
 }
 
 memory();
